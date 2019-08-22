7ea73adf0fb936491280ca66383f440183618423
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2019-08-22 13:41:09 -0500

    feat(add db context): add migrate at runtime

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -14,7 +17,27 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            
+            var host = CreateWebHostBuilder(args).Build();
+            
+            //Apply any pending migrations to the database
+            //if the db does not exist it will create the db as well
+            using(var scope = host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (System.Exception ex)
+                {
+                    
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occurred during migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
