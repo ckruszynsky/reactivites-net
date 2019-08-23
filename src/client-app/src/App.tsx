e886456b0e0d973152bfa159444c55d712c014b1
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2019-08-23 09:38:16 -0500

    feat(clientapp): add cors policy to api

--- src/client-app/src/App.tsx
+++ src/client-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+import axios from 'axios';
 import React, { Component } from 'react';
 
 import logo from './logo.svg';
@@ -10,20 +11,24 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.setState({
-      values: [{ id: 1, name: "Value 101" }, { id: 2, name: "Value 102" }]
+    axios.get("http://localhost:5000/api/values").then(response => {
+      this.setState({
+        values: response.data
+      });
     });
   }
 
   public render() {
-    const {values} = this.state;
+    const { values } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <ul>
-            {values.map((v:any)=> <li>{v.name}</li>)}
+            {values.map((v: any) => (
+              <li key={v.id}>{v.name}</li>
+            ))}
           </ul>
         </header>
       </div>
