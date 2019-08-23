7c0d52b544aac7adc9abb7c10337e784788b79db
Chad Kruszynsky <ckruszynsky@turner-industries.com>
2019-08-23 09:20:15 -0500

    feat(clientapp): add axios pkg and add values to app

--- src/client-app/src/App.tsx
+++ src/client-app/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      values: [{ id: 1, name: "Value 101" }, { id: 2, name: "Value 102" }]
+    });
+  }
+
+  public render() {
+    const {values} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {values.map((v:any)=> <li>{v.name}</li>)}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
