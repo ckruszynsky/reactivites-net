86b631a6c56fad79d098511bf40790c265be40ce
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-08-27 09:11:15 -0500

    feat(activities): add edit handler and command

--- src/API/Controllers/ActivitiesController.cs
+++ src/API/Controllers/ActivitiesController.cs
@@ -32,5 +32,11 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Create(Create.Command command){
             return await _mediator.Send(command);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id,Edit.Command command){
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
     }
 }
