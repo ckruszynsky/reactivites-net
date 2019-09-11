8c6e387560e2e3f4180fcc292d7410925bffb78e
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-10 20:16:53 -0500

    feat(styling): style activity list items

--- src/client-app/src/components/ActivitiyList/index.tsx
+++ src/client-app/src/components/ActivitiyList/index.tsx
@@ -1,10 +1,10 @@
 import './styles.scss';
 
-import React, {Fragment} from 'react';
-import {Item, Label, Segment} from 'semantic-ui-react';
+import React, { Fragment } from 'react';
+import { Item, Label } from 'semantic-ui-react';
 
-import {IActivity} from '../../models';
-import {ActivityListItem} from '../ActivityListItem';
+import { IActivity } from '../../models';
+import { ActivityListItem } from '../ActivityListItem';
 
 interface ActivityListProps {
   activities: [string, IActivity[]][];
@@ -16,17 +16,14 @@ export const ActivityList: React.FC<ActivityListProps> = ({
     <Fragment>
       {activities.map(([group, acts]) => (
         <Fragment key={group}>
-
           <Label size='large' color='blue'>
             {group}
           </Label>
-          <Segment clearing>
             <Item.Group divided>
               {acts.map((activity: IActivity) => (
                 <ActivityListItem key={activity.id} activity={activity} />
               ))}
             </Item.Group>
-          </Segment>
         </Fragment>
       ))}
 
