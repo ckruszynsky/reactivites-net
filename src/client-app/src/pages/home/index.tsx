3603980c0a40ae31846f39f48813c51a05ee25ad
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-11 21:41:35 -0500

    feat(styling):style home page

--- src/client-app/src/pages/home/index.tsx
+++ src/client-app/src/pages/home/index.tsx
@@ -1,28 +1,22 @@
 import './styles.scss';
 
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Container, Grid} from 'semantic-ui-react';
-
-import {PageHeader} from '../../components/PageHeader';
+import { Link } from 'react-router-dom';
+import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
 
 export const Home = () => {
   return (
-    <Container className="homePageContainer">
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={2}>
-            <PageHeader as="h2">Home</PageHeader>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <PageHeader as="h3">
-              Go to <Link to={"/activities"}>Activites</Link>
-            </PageHeader>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Container>
+         <Segment inverted textAlign='center' vertical className='masthead' >
+             <Container text>
+                 <Header as='h1' inverted>
+                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
+                     Reactivities
+                 </Header>
+                 <Header as='h2' inverted content='Welcome to Reactivities' />
+                 <Button as={Link} to='/activities' size='huge' inverted>
+                     Take me to the activities!
+                 </Button>
+             </Container>
+         </Segment>
   );
 };
