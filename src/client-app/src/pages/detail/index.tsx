77188115016be3d1e95fb6415a6e2195d4ee011d
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-11 21:35:43 -0500

    feat(styling): styled sidebar,chat and info

--- src/client-app/src/pages/detail/index.tsx
+++ src/client-app/src/pages/detail/index.tsx
@@ -5,7 +5,7 @@ import React, { useContext, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Container, Grid } from 'semantic-ui-react';
 
-import { Header,Info,Chat,Sidebar } from '../../components/Activity';
+import { Chat, Header, Info, Sidebar } from '../../components/Activity';
 import { LoadingIndicator } from '../../components/LoadingIndicator';
 import ActivityStore from '../../stores/activityStore';
 
@@ -13,32 +13,34 @@ interface DetailsParams {
   id: string;
 }
 
-export const Detail: React.FC<RouteComponentProps<DetailsParams>> = observer(({match, history}) => {
-  const activityStore = useContext(ActivityStore);
-  const {currentActivity, loadActivity, loading} = activityStore;
- 
-  const handleCancel = () => {
-    history.push('/activities');
-  }
-  useEffect(() => {
-    loadActivity(match.params.id);
-  }, [loadActivity, match.params.id]);
+export const Detail: React.FC<RouteComponentProps<DetailsParams>> = observer(
+  ({ match, history }) => {
+    const activityStore = useContext(ActivityStore);
+    const { currentActivity, loadActivity, loading } = activityStore;
 
-  if (loading || !currentActivity) {
-    return <LoadingIndicator content="Loading activity..." />;
+    const handleCancel = () => {
+      history.push("/activities");
+    };
+    useEffect(() => {
+      loadActivity(match.params.id);
+    }, [loadActivity, match.params.id]);
+
+    if (loading || !currentActivity) {
+      return <LoadingIndicator content="Loading activity..." />;
+    }
+    return (
+      <Container className="detailsContainer">
+        <Grid>
+          <Grid.Column width={10}>
+            <Header activity={currentActivity} />
+            <Info activity={currentActivity} />
+            <Chat activity={currentActivity} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Sidebar activity={currentActivity} />
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
   }
-  return (
-    <Container className="detailsContainer">
-      <Grid>
-        <Grid.Column width={10}>
-          <Header activity={currentActivity} />
-          <Info />
-          <Chat />
-        </Grid.Column>
-        <Grid.Column width={6}>
-            <Sidebar />
-        </Grid.Column>
-      </Grid>
-    </Container>
-  );
-});
+);
