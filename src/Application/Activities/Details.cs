b90ad0ac667efb1fc836e66d4f725b750078cc62
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-13 15:55:11 -0500

    feat(error handling): add middleware to pipeline

--- src/Application/Activities/Details.cs
+++ src/Application/Activities/Details.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -25,6 +27,15 @@ namespace Application.Activities
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
+                 if (activity == null)
+                {
+                    var errors = new
+                    {
+                        activity = $"Activity with id: ${request.Id} could not be found."
+                    };
+                    throw new RestException(HttpStatusCode.NotFound,errors);
+
+                }
                 return activity;
             }
         }
