b30cfea3cc5b35c525178d16ab39b3ef0a7cad49
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-13 21:51:32 -0500

    chore(master): setup react-widget-date-fns package

--- src/client-app/src/index.tsx
+++ src/client-app/src/index.tsx
@@ -1,14 +1,18 @@
 import './styles.scss';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-widgets/dist/css/react-widgets.css';
 import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dateFnsLocalizer from 'react-widgets-date-fns';
 
 import App from './pages/_app';
 import * as serviceWorker from './serviceWorker';
 import { Router } from './util/router';
 import { ScrollToTop } from './util/ScrollToTop';
+
+dateFnsLocalizer();
 
 ReactDOM.render(
     <Router>
