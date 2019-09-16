25799578a44ab80bed5c6121467eaa6dc4a26fad
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-09-16 09:03:52 -0500

    feat(identity): add signin configuration to startup

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,71 +1,67 @@
 ﻿using Application.Activities;
+using API.Middleware;
+using Domain;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence;
-using FluentValidation.AspNetCore;
-using API.Middleware;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(options =>
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (options => {
                 options
-                .UseSqlite
-                (
-                    Configuration.GetConnectionString("DefaultConnection")
-                );
+                    .UseSqlite (
+                        Configuration.GetConnectionString ("DefaultConnection")
+                    );
             });
 
-            services.AddCors(Options =>
-            {
-                Options.AddPolicy("CorsPolicy", policy =>
-                {
+            services.AddCors (Options => {
+                Options.AddPolicy ("CorsPolicy", policy => {
                     policy
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .WithOrigins("http://localhost:3000");
+                        .AllowAnyHeader ()
+                        .AllowAnyMethod ()
+                        .WithOrigins ("http://localhost:3000");
                 });
             });
 
-            services.AddMediatR
-            (
-                typeof(List.Handler).Assembly
+            services.AddMediatR (
+                typeof (List.Handler).Assembly
             );
 
-            services.AddMvc()
-            .AddFluentValidation
-            (
-                cfg => 
-                cfg.RegisterValidatorsFromAssemblyContaining<Create>()
-            )
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc ()
+                .AddFluentValidation (
+                    cfg =>
+                    cfg.RegisterValidatorsFromAssemblyContaining<Create> ()
+                )
+                .SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+
+            var builder = services.AddIdentityCore<AppUser> ();
+            var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext> ();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseMiddleware<ErrorHandlingMiddleware> ();
 
             //app.UseHttpsRedirection();
-            app.UseCors("CorsPolicy");
-            app.UseMvc();
+            app.UseCors ("CorsPolicy");
+            app.UseMvc ();
         }
     }
 }
