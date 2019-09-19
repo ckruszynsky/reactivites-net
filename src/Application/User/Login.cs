1998d7397a5952f0e9dcaae9041b5f24bf965ade
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-09-19 13:29:07 -0500

    feat(security): add user object for client response from api

--- src/Application/User/Login.cs
+++ src/Application/User/Login.cs
@@ -6,12 +6,11 @@ using Domain;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
-
 namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -19,41 +18,42 @@ namespace Application.User
 
         public class QueryValidator : AbstractValidator<Query>
         {
-
             public QueryValidator ()
             {
                 RuleFor (x => x.Email).NotEmpty ();
                 RuleFor (x => x.Password).NotEmpty ();
             }
-
         }
+
         public class List
         {
-            public class Handler : IRequestHandler<Query, AppUser>
+            public class Handler : IRequestHandler<Query, User>
             {
                 private readonly UserManager<AppUser> _userManager;
                 private readonly SignInManager<AppUser> _signInManager;
-
                 public Handler (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
                 {
                     _signInManager = signInManager;
                     _userManager = userManager;
-
                 }
-
-                public async Task<AppUser> Handle (Query request, CancellationToken cancellationToken)
+                public async Task<User> Handle (Query request, CancellationToken cancellationToken)
                 {
                     var user = await _userManager.FindByEmailAsync (request.Email);
                     if (user == null)
                     {
                         throw new RestException (HttpStatusCode.Unauthorized);
                     }
-
                     var result = await _signInManager.CheckPasswordSignInAsync (user, request.Password, false);
                     if (result.Succeeded)
                     {
                         //TODO: generate token
-                        return user;
+                        return new User
+                        {
+                            DisplayName = user.DisplayName,
+                                Token = "This will be a token",
+                                Username = user.UserName,
+                                Image = null
+                        };
                     }
                     else
                     {
