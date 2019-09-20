84812ca90fae2916cb86b1e2d38e80451e0d5ce9
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-09-20 06:13:17 -0500

    feat(security): add jwtgenerator logic to generate token

--- src/Infrastructure/Security/JwtGenerator.cs
+++ src/Infrastructure/Security/JwtGenerator.cs
@@ -1,14 +1,39 @@
-
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Application.Contracts;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
     public class JwtGenerator : IJwtGenerator
     {
-        public string CreateToken(AppUser user)
+        public string CreateToken (AppUser user)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim (JwtRegisteredClaimNames.NameId, user.UserName) //adds username as the NameId inside our token
+            };
+
+            //generate signing credentials
+            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes ("a very super secret key")); //TODO: store this as secret on project and not hard coded
+            var credentials = new SigningCredentials (key, SecurityAlgorithms.HmacSha512Signature);
+
+            //create token Descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity (claims),
+                Expires = DateTime.Now.AddDays (7), //TODO: Adjust expiry date after development
+                SigningCredentials = credentials
+            };
+
+            //generate token
+            var tokenHandler = new JwtSecurityTokenHandler ();
+            var token = tokenHandler.CreateToken (tokenDescriptor);
+            return tokenHandler.WriteToken (token);
         }
     }
 }
