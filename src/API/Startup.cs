eb490143eae1094610928e339d695df5b6621a10
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-09-20 13:28:23 -0500

    feat(security): add auth policy

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -7,10 +7,12 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -54,7 +56,16 @@ namespace API
                 typeof (List.Handler).Assembly
             );
 
-            services.AddMvc ()
+            services.AddMvc (opt =>
+                {
+                    //adds so that any requests made will need to be authorized.
+                    var policy = new AuthorizationPolicyBuilder ()
+                        .RequireAuthenticatedUser ()
+                        .Build ();
+
+                    opt.Filters.Add (new AuthorizeFilter (policy));
+
+                })
                 .AddFluentValidation (
                     cfg =>
                     cfg.RegisterValidatorsFromAssemblyContaining<Create> ()
