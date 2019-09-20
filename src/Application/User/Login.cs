2a10dab8c158140a37c56a9c8d1208599c28277b
Chad Kruszynsky <chadkruszynsky@Chads-MacBook-Pro.local>
2019-09-20 14:31:08 -0500

    feat(security): add getting current logged in user

--- src/Application/User/Login.cs
+++ src/Application/User/Login.cs
@@ -19,48 +19,45 @@ namespace Application.User
 
         public class QueryValidator : AbstractValidator<Query>
         {
-            public QueryValidator ()
+            public QueryValidator()
             {
-                RuleFor (x => x.Email).NotEmpty ();
-                RuleFor (x => x.Password).NotEmpty ();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
             }
         }
 
-        public class List
+        public class Handler : IRequestHandler<Query, User>
         {
-            public class Handler : IRequestHandler<Query, User>
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IJwtGenerator jwtGenerator)
             {
-                private readonly UserManager<AppUser> _userManager;
-                private readonly SignInManager<AppUser> _signInManager;
-                private readonly IJwtGenerator _jwtGenerator;
-                public Handler (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IJwtGenerator jwtGenerator)
+                _jwtGenerator = jwtGenerator;
+                _signInManager = signInManager;
+                _userManager = userManager;
+            }
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByEmailAsync(request.Email);
+                if (user == null)
                 {
-                    _jwtGenerator = jwtGenerator;
-                    _signInManager = signInManager;
-                    _userManager = userManager;
+                    throw new RestException(HttpStatusCode.Unauthorized);
                 }
-                public async Task<User> Handle (Query request, CancellationToken cancellationToken)
+                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+                if (result.Succeeded)
                 {
-                    var user = await _userManager.FindByEmailAsync (request.Email);
-                    if (user == null)
+                    return new User
                     {
-                        throw new RestException (HttpStatusCode.Unauthorized);
-                    }
-                    var result = await _signInManager.CheckPasswordSignInAsync (user, request.Password, false);
-                    if (result.Succeeded)
-                    {
-                        return new User
-                        {
-                            DisplayName = user.DisplayName,
-                                Token = _jwtGenerator.CreateToken (user),
-                                Username = user.UserName,
-                                Image = null
-                        };
-                    }
-                    else
-                    {
-                        throw new RestException (HttpStatusCode.Unauthorized);
-                    }
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        Username = user.UserName,
+                        Image = null
+                    };
+                }
+                else
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized);
                 }
             }
         }
