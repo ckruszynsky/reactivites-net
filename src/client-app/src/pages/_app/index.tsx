bc13c71740078bc5d47618a8d631e1c4b31161dd
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-09-23 13:45:55 -0500

    feat(client-login): persist login state across page refreshes

--- src/client-app/src/pages/_app/index.tsx
+++ src/client-app/src/pages/_app/index.tsx
@@ -1,12 +1,13 @@
 import './styles.scss';
 
 import {observer} from 'mobx-react-lite';
-import React, {Fragment, useContext} from 'react';
+import React, {Fragment, useContext, useEffect} from 'react';
 import {Route, RouteComponentProps, Switch, withRouter} from 'react-router';
 import {ToastContainer} from 'react-toastify';
 import {Container} from 'semantic-ui-react';
 
 import {NotFound} from '../../components/Errors';
+import {LoadingIndicator} from '../../components/LoadingIndicator';
 import {Navbar} from '../../components/Navbar';
 import {RootStoreContext} from '../../stores/rootStore';
 import {Dashboard} from '../dashboard';
@@ -17,7 +18,22 @@ import {NewActivity} from '../newActivity';
 
 const App: React.FC<RouteComponentProps> = observer(({location}) => {
   const rootStore = useContext(RootStoreContext);
-  const {isLoggedIn, user, logout} = rootStore.userStore;
+  const {isLoggedIn, user, logout, getUser} = rootStore.userStore;
+  const {setAppLoaded, token, appLoaded} = rootStore.commonStore;
+
+
+  //check to see if we have a token, and if we do 
+  //then we will go and fetch the user from our api
+  //and if we don't then we just set the app loaded
+  useEffect(() => {
+    if (token) {
+      getUser().finally(() => setAppLoaded());
+    } else {
+      setAppLoaded();
+    }
+  }, [getUser, setAppLoaded, token]);
+
+  if (!appLoaded) return <LoadingIndicator content='Loading Application' />
 
   return (
     <Fragment>
