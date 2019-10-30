51229e2dd4cbe1ec1a4f48d1dbdf971d9cfd9f9d
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-10-30 09:36:00 -0500

    feat(profile): isloate loading indicators

--- src/client-app/src/components/Profile/PhotoListItem.tsx
+++ src/client-app/src/components/Profile/PhotoListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Card, Image} from 'semantic-ui-react';
 
 import {IPhoto} from '../../models/profile';
@@ -6,19 +6,38 @@ import {IPhoto} from '../../models/profile';
 
 export const PhotoListItem: React.FC<{
     photo: IPhoto,
-    isLoggedInUserProfile:boolean,
-    setMainPhoto: (photo:IPhoto)=> Promise<void>,
-    deletePhoto: (photo:IPhoto) => Promise<void>, 
-    loading:boolean}> = ({
-        photo, 
-        isLoggedInUserProfile,
-        setMainPhoto,
-        deletePhoto,
-        loading}) => (<Card key={photo.id}>
-    <Image src={photo.url} />
-    {isLoggedInUserProfile &&
-        <Button.Group   size="small">       
-            <Button inverted color="pink" icon="plus" onClick={()=> setMainPhoto(photo)} loading={loading} />
-            <Button inverted color="purple" icon="trash" onClick={()=> deletePhoto(photo)} />            
-        </Button.Group>}
-</Card>);
+    isLoggedInUserProfile: boolean,
+    setMainPhoto: (photo: IPhoto) => Promise<void>,
+    deletePhoto: (photo: IPhoto) => Promise<void>,
+    loading: boolean
+}> = ({
+    photo,
+    isLoggedInUserProfile,
+    setMainPhoto,
+    deletePhoto,
+    loading}) => {
+        const [target,setTarget] = useState<string|undefined>();
+
+        return (<Card key={photo.id}>
+            <Image src={photo.url} />
+            {isLoggedInUserProfile &&
+                <Button.Group size="small">
+                    <Button name={photo.id}
+                        inverted color="pink"
+                        icon="plus"
+                        onClick={(e) => { 
+                            setTarget(e.currentTarget.name); 
+                            setMainPhoto(photo)
+                        }
+                    } 
+                    loading={loading && target === photo.id}
+                    disabled={photo.isMain}
+                    />
+                    <Button inverted 
+                        color="purple" 
+                        icon="trash" 
+                        onClick={() => deletePhoto(photo)}
+                        disabled={photo.isMain} />
+                </Button.Group>}
+        </Card>)
+    };
