cd22d5da027ab48608354b2a8210983f40d53702
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-12-03 12:23:55 -0600

    feat(following): added following entity and relationships

--- src/Domain/AppUser.cs
+++ src/Domain/AppUser.cs
@@ -9,5 +9,9 @@ namespace Domain
         public string Bio { get; set; }
         public ICollection<UserActivity> UserActivities { get; set; }
         public virtual ICollection<Photo> Photos { get; set; } = new List<Photo>();
+
+        public ICollection<UserFollowing> Followings {get;set;} = new List<UserFollowing>();
+        public ICollection<UserFollowing> Followers { get; set; } = new List<UserFollowing>();
+
     }
 }
