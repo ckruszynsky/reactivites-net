75f578af55cc5edebf1a73514b1e510647de6bf1
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-12-06 06:53:13 -0600

    feat(filtering): add filtering to API for activities

--- src/Application/Activities/List.cs
+++ src/Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -20,21 +21,30 @@ namespace Application.Activities
         }
         public class Query : IRequest<ActivitiesEnvelope>
         {
-            public Query (int? limit, int? offset)
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime? StartDate { get; set; }
+            public Query (int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
+                StartDate = startDate ?? DateTime.Now;
+                IsHost = isHost;
+                IsGoing = isGoing;
                 Limit = limit;
                 Offset = offset;
 
             }
-            public int? Limit { get; set; }
-            public int? Offset { get; set; }
+
         }
         public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly IDbContextResolver _contextResolver;
             private readonly IMapper _mapper;
-            public Handler (IDbContextResolver contextResolver, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler (IDbContextResolver contextResolver, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _contextResolver = contextResolver;
 
@@ -43,20 +53,41 @@ namespace Application.Activities
             public async Task<ActivitiesEnvelope> Handle (Query request, CancellationToken cancellationToken)
             {
                 var context = _contextResolver.GetContext ();
-                var queryable = context.Set<Activity> ().AsQueryable ();
-
-                var activities = await queryable
-                    .Skip (request.Offset ?? 0)
-                    .Take (request.Limit ?? 3)
+                var currentUserName = _userAccessor.GetCurrentUsername();
+                var queryable = context.Set<Activity> ()
                     .Include (ua => ua.UserActivities)
                     .ThenInclude (au => au.AppUser)
                     .ThenInclude (ap => ap.Photos)
+                    .Where (x => x.Date >= request.StartDate)
+                    .OrderBy (x => x.Date)
+                    .AsQueryable ();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable.Where (
+                        x => x.UserActivities.Any(
+                            a=> a.AppUser.UserName == currentUserName
+                        )
+                    );
+                }
+
+                if(request.IsHost && !request.IsGoing){
+                    queryable = queryable.Where (
+                        x => x.UserActivities.Any(
+                            a=> a.AppUser.UserName == currentUserName && a.IsHost
+                        )
+                    );
+                }
+
+                var activities = await queryable
+                    .Skip (request.Offset ?? 0)
+                    .Take (request.Limit ?? 3)                   
                     .ToListAsync (cancellationToken);
 
                 return new ActivitiesEnvelope
                 {
                     Activities = _mapper.Map<List<Activity>, List<ActivityDto>> (activities),
-                    Count = queryable.Count()
+                        Count = queryable.Count ()
                 };
             }
         }
